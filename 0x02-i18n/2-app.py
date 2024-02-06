9c1bf2f5355e3917252c099201c8a7a50f880a0c
Bantamlak <bantamlak29@gmail.com>
2024-02-06 19:33:30 +0300

    port and host removed

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""File: 1-app.py
+"""
+File: 1-app.py
+
+Basic Babel setup
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -7,7 +10,8 @@ from typing import List
 
 
 class Config:
-    """Config
+    """
+    Configures available languages
     """
     LANGUAGES: List[str] = ["en", "fr"]
     BABEL_DEFAULT_LOCALE: str = "en"
@@ -21,16 +25,21 @@ babel: Babel = Babel(app)
 
 @app.route('/')
 def home() -> str:
-    """Home"""
+    """
+    Returns:
+    - The html file
+    """
     return render_template('2-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ Best language maych
+    """
+    Returns:
+    - Best language match
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
