c985d5bf223312997c2f8d9eeab7b99b8b20d45f
Bantamlak <bantamlak29@gmail.com>
2024-02-06 19:29:23 +0300

    documentation

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""File: 1-app.py
+"""
+File: 1-app.py
+
+Basic Babel setup
 """
 from flask import Flask, render_template
 from flask_babel import Babel
@@ -7,7 +10,8 @@ from typing import List
 
 
 class Config:
-    """Config
+    """
+    Configures available languages
     """
     LANGUAGES: List[str] = ["en", "fr"]
     BABEL_DEFAULT_LOCALE: str = "en"
@@ -21,9 +25,12 @@ babel: Babel = Babel(app)
 
 @app.route('/')
 def home() -> str:
-    """Home"""
+    """
+    Returns:
+    - The html file
+    """
     return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
