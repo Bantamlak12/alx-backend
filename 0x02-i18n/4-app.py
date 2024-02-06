1daf283e35f5981793bbb7a1ab728ea6a5b95f3c
Bantamlak <bantamlak29@gmail.com>
2024-02-06 22:01:59 +0300

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+File: 1-app.py
+
+Basic Babel setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import List
+
+
+class Config:
+    """
+    Configures available languages
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app: Flask = Flask(__name__)
+app.config.from_object(Config)
+babel: Babel = Babel(app)
+
+
+@app.route('/')
+def home() -> str:
+    """
+    Returns:
+    - The html file
+    """
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Returns:
+    - Best language match
+    """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
