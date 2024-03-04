e6abbc8227bc7a06f2933438d321e6b436e70b80
Bantamlak <bantamlak29@gmail.com>
2024-03-04 18:38:32 +0300

    task 6: job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,26 @@
+const kue = require('kue');
+
+// Create a queue
+const queue = kue.createQueue();
+
+// Job object
+const obj = {
+  phoneNumber: '232435435345',
+  message: 'This is a test notification',
+};
+
+// Create a queue named 'push_notification_code' and create a job
+const job = queue.create('push_notification_code', obj).save(() => {
+  // Job successful creation
+  console.log(`Notification job created: ${job.id}`);
+});
+
+// Job completion
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+// Job failure
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
